41d2445039ad8325de7ba0cdc0dd446b8d0e9ba1
Edgar Omar Sanchez Farias <edgaromarsf@gmail.com>
2023-10-30 16:02:34 -0600

    update settings

--- django_crm/settings.py
+++ django_crm/settings.py
@@ -27,6 +27,8 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
 
     'website.apps.WebsiteConfig',
+
+    'django_seed',
 ]
 
 MIDDLEWARE = [
@@ -106,6 +108,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = 'static/'
+STATIC_ROOT = 'static/'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
